eb9475281069da35c03268d46418fb19ed758ed8
mkushner <maxim.kushner@gmail.com>
2021-06-29 13:05:45 +0300

    v1

---
+++ src/main.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, Response
+import requests
+
+app = Flask(__name__)
+
+# handling a /webhook route for POST command
+@app.route('/webhook', methods=['POST'])
+
+def respond():
+    
+    print(request.json)
+    k = request.json
+
+    # put up-to-date keys for parsing from Pipedrive JSON
+    slack_data = '{"text":"' + k['meta']['object'] + ' ' + k['meta']['action'] + ': ' + k['current']['title'] + '"}'
+    
+    # put up-to-date slack webhook URL
+    url = #slack_webhook_URL
+    response = requests.post(url, slack_data)
+    
+    return Response(status=200)
+
+# handling default route for a health check
+@app.route('/')
+def index():
+    return "<h1>slack notifications here</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=False)
